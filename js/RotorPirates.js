5b35b9acd4fe491784678619d36db41484990629
Kenneth Mitchell <kmitchel46725@gmail.com>
2020-02-24 18:23:03 -0500

    Acutal Rates
    Commpare BF rates vs _Actual_ rate system.

--- js/RotorPirates.js
+++ js/RotorPirates.js
@@ -2,48 +2,21 @@
 
 
 // defaults
-// RF
-rfRate = 400
-rfExpo = 50
-rfAcro = 140
 
 // BF
-bfRate=1.0
+bfRate=1
 bfSuper=0.7
-bfExpo=0.0
+bfExpo=0
 
-// KISS
-kissRcRate=0.7
-kissRate=0.7
-kissCurve=0.4
-
-
-// KISS rate function
-var kscalc = function(rcCommand, rate, rcCurve, rcRate) {
-    kissRpyUseRates = 1 - Math.abs(rcCommand) * rate
-    kissRxRaw = rcCommand * 1000
-    kissTempCurve = (kissRxRaw * kissRxRaw / 1000000)
-    rcCommand = ((rcCommand * kissTempCurve) * rcCurve + rcCommand * (1 - rcCurve)) * (rcRate / 10)
-    kissAngle = ((2000.0 * (1.0 / kissRpyUseRates)) * rcCommand) // setpoint is calculated directly here
-    return kissAngle
-}
-
-
-// RF rate function
-var rfcalc = function(rcCommand, rate, expo, acrop) {
-    returnValue = ((1 + 0.01 * expo * (rcCommand * rcCommand - 1.0)) * rcCommand)
-    returnValue = (returnValue * (rate + (Math.abs(returnValue) * rate * acrop * 0.01)))
-    return returnValue
-}
-
+aRate=1
+aSuper=0.7
+aExpo=0
 
 // BF rate calculation function
 var bfcalc = function(rcCommand, rcRate, expo, superRate) {
     var clamp = function(n, minn, maxn) {
         return Math.max(Math.min(maxn, n), minn);
     }
-
-    var absRcCommand = Math.abs(rcCommand);
 
     if(rcRate > 2.0)
         rcRate = rcRate + (14.54 * (rcRate - 2.0))
@@ -53,121 +26,72 @@ var bfcalc = function(rcCommand, rcRate, expo, superRate) {
 
     angleRate = 200.0 * rcRate * rcCommand;
     if(superRate != 0){
-        rcSuperFactor = 1.0 / (clamp(1.0 - absRcCommand * (superRate)), 0.01, 1.00))
+        rcSuperFactor = 1.0 / (clamp(1.0 - (Math.abs(rcCommand) * (superRate)), 0.01, 1.00))
         angleRate *= rcSuperFactor
     }
 
     return angleRate
 }
 
+// Actual rate calculation function
+var acalc = function(rcCommandf, rcRates, rcExpo, rates) {
+
+    var expof = rcExpo;
+    rcRates = rcRates * 100;
+    rates = rates * 100;
+    rcCommandfAbs = Math.abs(rcCommandf)
+    expof = rcCommandfAbs * (Math.pow(rcCommandf, 5) * expof + rcCommandf * (1 - expof))
+
+    var centerSensitivity = rcRates * 10.0
+    var stickMovement = Math.max(0, rates * 10.0 - centerSensitivity)
+    var angleRate = rcCommandf * centerSensitivity + stickMovement * expof
+
+    return angleRate
+}
+
 var reset_button_on_clicked = function() {
-    rfrate_slider.reset()
-    rfexpo_slider.reset()
-    rfacrop_slider.reset()
     bfrate_slider.reset()
     bfexpo_slider.reset()
     bfsuper_slider.reset()
-    ksrate_slider.reset()
-    kscurve_slider.reset()
-    ksrcrate_slider.reset()
-}
-function calcFromRF() {
-    var ratesAPI = "https://api.apocolipse.com/rates?";
-    $.getJSON( ratesAPI, {
-        t: "rf",
-        p1: parseFloat($('#rfRate').val(), 10),
-        p2: parseFloat($('#rfExpo').val(), 10),
-        p3: parseFloat($('#rfAcro').val(), 10)
-      })
-        .done(function( data ) {
-            console.log(data);
-            data = data.replace(/\'/g, '"');
-            handleBruteForceResponse(JSON.parse(data));
-
-        });
+    arate_slider.reset()
+    aexpo_slider.reset()
+    asuper_slider.reset()
 }
 
-function calcFromBF() {
-    var ratesAPI = "https://api.apocolipse.com/rates?";
-    $.getJSON( ratesAPI, {
-        t: "bf",
-        p1: parseFloat($('#bfRate').val(), 10),
-        p2: parseFloat($('#bfExpo').val(), 10),
-        p3: parseFloat($('#bfSuper').val(), 10)
-      })
-        .done(function( data ) {
-            console.log(data);
-            data = data.replace(/\'/g, '"');
-            handleBruteForceResponse(JSON.parse(data));
 
-        });
-}
-
-function calcFromKiss() {
-    var ratesAPI = "https://api.apocolipse.com/rates?";
-    $.getJSON( ratesAPI, {
-        t: "ks",
-        p1: parseFloat($('#kissRate').val(), 10),
-        p2: parseFloat($('#kissCurve').val(), 10),
-        p3: parseFloat($('#kissRcRate').val(), 10)
-      })
-        .done(function( data ) {
-            console.log(data);
-            data = data.replace(/\'/g, '"');
-            handleBruteForceResponse(JSON.parse(data));
-
-        });
-}
-
-function handleBruteForceResponse(data) {
-    $('#rfRate').val(parseFloat(data['rfRate'], 10)).change();
-    $('#rfExpo').val(parseFloat(data['rfexpo'], 10)).change();
-    $('#rfAcro').val(parseFloat(data['rfacroPlus'], 10)).change();
-    $('#bfRate').val(parseFloat(data['bfrcRate'], 10)).change();
-    $('#bfExpo').val(parseFloat(data['bfexpo'], 10)).change();
-    $('#bfSuper').val(parseFloat(data['bfsuperRate'], 10)).change();
-    $('#kissRate').val(parseFloat(data['ksrate'], 10)).change();
-    $('#kissCurve').val(parseFloat(data['ksrcCurve'], 10)).change();
-    $('#kissRcRate').val(parseFloat(data['ksrcRate'], 10)).change();
-}
 function drawChart() {
-    rfRate = parseFloat($('#rfRate').val(), 10);
-    rfExpo = parseFloat($('#rfExpo').val(), 10);
-    rfAcro = parseFloat($('#rfAcro').val(), 10);
     bfRate = parseFloat($('#bfRate').val(), 10);
     bfExpo = parseFloat($('#bfExpo').val(), 10);
     bfSuper = parseFloat($('#bfSuper').val(), 10);
-    kissRate = parseFloat($('#kissRate').val(), 10);
-    kissCurve = parseFloat($('#kissCurve').val(), 10);
-    kissRcRate = parseFloat($('#kissRcRate').val(), 10);
-
-    // console.log(rfRate, rfExpo, rfAcro);
-    // console.log(bfRate, bfExpo, bfSuper);
-    // console.log(kissRate, kissCurve, kissRcRate);
+    aRate = parseFloat($('#aRate').val(), 10);
+    aExpo = parseFloat($('#aExpo').val(), 10);
+    aSuper = parseFloat($('#aSuper').val(), 10);
+     console.log(bfRate, bfExpo, bfSuper);
+    // console.log(aRate, aExpo, aSuper);
 
     var ratesData = [];
 
     var data = new google.visualization.DataTable();
     data.addColumn('number', 'Stick Input');
-    data.addColumn('number', 'RF');
     data.addColumn('number', 'BF');
-    data.addColumn('number', 'KISS');
+    data.addColumn('number', 'Actual');
+    data.addColumn('number', 'Actual - Center Sensitivity');
 
     for (var i = 0; i <= 1; i+=0.01) {
         data.addRow([
             i,
-            rfcalc(i, rfRate, rfExpo, rfAcro),
             bfcalc(i, bfRate, bfExpo, bfSuper),
-            kscalc(i, kissRate, kissCurve, kissRcRate)
+            acalc(i, aRate, aExpo, aSuper),
+            acalc(i, aRate, 0, 0)
         ]);
     }
 
     var i = 1;
     data.addRow([
         i,
-        rfcalc(i, rfRate, rfExpo, rfAcro),
         bfcalc(i, bfRate, bfExpo, bfSuper),
-        kscalc(i, kissRate, kissCurve, kissRcRate)
+        acalc(i, aRate, aExpo, aSuper),
+        acalc(i, aRate, 0, 0)
     ]);
 
     var options = {
@@ -188,7 +112,9 @@ function drawChart() {
         vAxis: {
             baselineColor: "#888",
             gridlines: {color: "#888"},
-            textStyle: {color: "#888"}
+            textStyle: {color: "#888"},
+            minValue: 0,
+            maxValue: 800
         }
     };
 
@@ -201,17 +127,6 @@ $( document ).ready(function() {
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
 
-
-    $('#rfRateSlider').on('input', function() {
-        $('#rfRate').val(this.value).change();
-    });
-    $('#rfExpoSlider').on('input', function() {
-        $('#rfExpo').val(this.value).change();
-    });
-    $('#rfAcroSlider').on('input', function() {
-        $('#rfAcro').val(this.value).change();
-    });
-
     $('#bfRateSlider').on('input', function() {
         $('#bfRate').val(this.value).change();
     });
@@ -222,27 +137,6 @@ $( document ).ready(function() {
         $('#bfSuper').val(this.value).change();
     });
 
-    $('#kissRcRateSlider').on('input', function() {
-        $('#kissRcRate').val(this.value).change();
-    });
-    $('#kissRateSlider').on('input', function() {
-        $('#kissRate').val(this.value).change();
-    });
-    $('#kissCurveSlider').on('input', function() {
-        $('#kissCurve').val(this.value).change();
-    });
-
-
-
-    $('#rfRate').on('input change paste keyup', function() {
-        $('#rfRateSlider').val(this.value).change();
-    });
-    $('#rfExpo').on('input', function() {
-        $('#rfExpoSlider').val(this.value).change();
-    });
-    $('#rfAcro').on('input', function() {
-        $('#rfAcroSlider').val(this.value).change();
-    });
 
     $('#bfRate').on('input', function() {
         $('#bfRateSlider').val(this.value).change();
@@ -254,14 +148,25 @@ $( document ).ready(function() {
         $('#bfSuperSlider').val(this.value).change();
     });
 
-    $('#kissRcRate').on('input', function() {
-        $('#kissRcRateSlider').val(this.value).change();
+    $('#aRateSlider').on('input', function() {
+        $('#aRate').val(this.value).change();
     });
-    $('#kissRate').on('input', function() {
-        $('#kissRateSlider').val(this.value).change();
+    $('#aExpoSlider').on('input', function() {
+        $('#aExpo').val(this.value).change();
     });
-    $('#kissCurve').on('input', function() {
-        $('#kissCurveSlider').val(this.value).change();
+    $('#aSuperSlider').on('input', function() {
+        $('#aSuper').val(this.value).change();
+    });
+
+
+    $('#aRate').on('input', function() {
+        $('#aRateSlider').val(this.value).change();
+    });
+    $('#aExpo').on('input', function() {
+        $('#aExpoSlider').val(this.value).change();
+    });
+    $('#aSuper').on('input', function() {
+        $('#aSuperSlider').val(this.value).change();
     });
 
 
